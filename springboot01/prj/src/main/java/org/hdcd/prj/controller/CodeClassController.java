a5c824c02955a23f683ee311cdeedc8e2a64703a
kfx2064 <jth454@naver.com>
2023-10-24 09:55:49 +0900

    study spring boot initializing.

---
+++ springboot01/prj/src/main/java/org/hdcd/prj/controller/CodeClassController.java
@@ -0,0 +1,13 @@
+package org.hdcd.prj.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/codeclass")
+public class CodeClassController {
+	
+	
+
+}
