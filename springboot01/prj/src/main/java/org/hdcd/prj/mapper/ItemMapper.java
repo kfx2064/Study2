90d4f53181c57e4c1f5bf1777a230051a94913c7
kfx2064 <jth454@naver.com>
2023-10-25 13:34:12 +0900

    make front back.

--- springboot01/prj/src/main/java/org/hdcd/prj/mapper/ItemMapper.java
+++ springboot01/prj/src/main/java/org/hdcd/prj/mapper/ItemMapper.java
@@ -1,4 +1,22 @@
 package org.hdcd.prj.mapper;
 
+import org.hdcd.prj.domain.Item;
+
+import java.util.List;
+
 public interface ItemMapper {
+
+    public void create(Item item) throws Exception;
+
+    public Item read(Integer itemId) throws Exception;
+
+    public void update(Item item) throws Exception;
+
+    public void delete(Integer itemId) throws Exception;
+
+    public List<Item> list() throws Exception;
+
+    public String getPicture(Integer itemId) throws Exception;
+
+    public String getPreview(Integer itemId) throws Exception;
 }
--- springboot01/prj/src/main/java/org/hdcd/prj/service/ItemService.java
+++ springboot01/prj/src/main/java/org/hdcd/prj/service/ItemService.java
@@ -1,4 +1,23 @@
 package org.hdcd.prj.service;
 
+import org.hdcd.prj.domain.Item;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public interface ItemService {
+
+    public void register(Item item) throws Exception;
+
+    public Item read(Integer itemId) throws Exception;
+
+    public void modify(Item item) throws Exception;
+
+    public void remove(Integer itemId) throws Exception;
+
+    public List<Item> list() throws Exception;
+
+    public String getPicture(Integer itemId) throws Exception;
+
+    public String getPreview(Integer itemId) throws Exception;
 }
