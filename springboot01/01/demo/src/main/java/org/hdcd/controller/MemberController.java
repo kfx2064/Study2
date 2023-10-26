c018feedf007212d4ad93719cd4b75d9c4e4d339
kfx2064 <jth454@naver.com>
2023-10-26 18:04:10 +0900

    study spring boot.

--- springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
@@ -1,16 +1,17 @@
 package org.hdcd.controller;
 
-import org.hdcd.domain.Member;
+import org.hdcd.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -24,32 +25,118 @@ public class MemberController {
         return "registerForm";
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(Member member) {
-        logger.info("register");
-        logger.info("member.getUserId() = " + member.getUserId());
-        logger.info("member.getPassword() = " + member.getPassword());
-        logger.info("member.getDateOfBirth() = " + member.getDateOfBirth());
+    @RequestMapping(value = "/registerFile01", method = RequestMethod.POST)
+    public String registerFile01(MultipartFile picture) throws Exception {
+        logger.info("registerFile01");
+
+        logger.info("originalName: " + picture.getOriginalFilename());
+        logger.info("size: " + picture.getSize());
+        logger.info("contentType: " + picture.getContentType());
 
         return "success";
     }
 
-    @RequestMapping(value = "/registerByGet01", method = RequestMethod.GET)
-    public String registerByGet01(String userId, @DateTimeFormat(pattern = "yyyyMMdd") Date dateOfBirth) {
-        logger.info("registerByGet01");
+    @RequestMapping(value = "/registerFile02", method = RequestMethod.POST)
+    public String registerFile02(String userId, String password, MultipartFile picture) throws Exception {
+        logger.info("registerFile02");
         logger.info("userId = " + userId);
-        logger.info("dateOfBirth = " + dateOfBirth);
+        logger.info("password = " + password);
+        logger.info("originalName: " + picture.getOriginalFilename());
+        logger.info("size: " + picture.getSize());
+        logger.info("contentType: " + picture.getContentType());
 
         return "success";
     }
 
-    @RequestMapping(value = "/registerByGet02", method = RequestMethod.GET)
-    public String registerByGet02(Member member) {
-        logger.info("registerByGet02");
-        logger.info("member.getUserId() = " + member.getUserId());
-        logger.info("member.getDateOfBirth() = " + member.getDateOfBirth());
+    @RequestMapping(value = "/registerFile03", method = RequestMethod.POST)
+    public String registerFile03(Member member, MultipartFile picture) throws Exception {
+        logger.info("registerFile03");
+
+        logger.info("userId = " + member.getUserId());
+        logger.info("password = " + member.getPassword());
+
+        logger.info("originalName: " + picture.getOriginalFilename());
+        logger.info("size: " + picture.getSize());
+        logger.info("contentType: " + picture.getContentType());
 
         return "success";
     }
 
+    @RequestMapping(value = "/registerFile04", method = RequestMethod.POST)
+    public String registerFile04(FileMember fileMember) throws Exception {
+        logger.info("registerFile04");
+
+        logger.info("userId = " + fileMember.getUserId());
+        logger.info("password = " + fileMember.getPassword());
+
+        MultipartFile picture = fileMember.getPicture();
+
+        logger.info("originalName: " + picture.getOriginalFilename());
+        logger.info("size: " + picture.getSize());
+        logger.info("contentType: " + picture.getContentType());
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/registerFile05", method = RequestMethod.POST)
+    public String registerFile05(MultipartFile picture, MultipartFile picture2) throws Exception {
+        logger.info("registerFile05");
+
+        logger.info("picture originalName: " + picture.getOriginalFilename());
+        logger.info("picture size: " + picture.getSize());
+        logger.info("picture contentType: " + picture.getContentType());
+
+        logger.info("picture2 originalName: " + picture2.getOriginalFilename());
+        logger.info("picture2 size: " + picture2.getSize());
+        logger.info("picture2 contentType: " + picture2.getContentType());
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/registerFile06", method = RequestMethod.POST)
+    public String registerFile06(List<MultipartFile> pictureList) throws Exception {
+        logger.info("registerFile06");
+
+        logger.info("registerFile06 pictureList.size() = " + pictureList.size());
+
+        for (MultipartFile picture : pictureList) {
+            logger.info("picture originalName: " + picture.getOriginalFilename());
+            logger.info("picture size: " + picture.getSize());
+            logger.info("picture contentType: " + picture.getContentType());
+        }
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/registerFile07", method = RequestMethod.POST)
+    public String registerFile07(MultiFileMember multiFileMember) throws Exception {
+        logger.info("registerFile07");
+
+        List<MultipartFile> pictureList = multiFileMember.getPictureList();
+
+        logger.info("registerFile07 pictureList.size() = " + pictureList.size());
+
+        for (MultipartFile picture : pictureList) {
+            logger.info("picture originalName: " +picture.getOriginalFilename());
+            logger.info("picture size: " + picture.getSize());
+            logger.info("picture contentType: " + picture.getContentType());
+        }
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/registerFile08", method = RequestMethod.POST)
+    public String registerFile08(MultipartFile[] pictureList) throws Exception {
+        logger.info("registerFile08");
+
+        logger.info("registerFile08 pictureList.length = " + pictureList.length);
+
+        for (MultipartFile picture : pictureList) {
+            logger.info("picture originalName: " + picture.getOriginalFilename());
+            logger.info("picture size: " + picture.getSize());
+            logger.info("picture contentType: " + picture.getContentType());
+        }
+
+        return "success";
+    }
 }
