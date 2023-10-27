649a36d051af77d17b267c06a381b5e4380ef7db
kfx2064 <jth454@naver.com>
2023-10-27 10:52:51 +0900

    study spring boot.

--- springboot01/01/demo/src/main/java/org/hdcd/controller/HomeController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/HomeController.java
@@ -24,59 +24,11 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@ResponseBody
-	@RequestMapping(value = "/goHome1101", method = RequestMethod.GET)
-	public ResponseEntity<byte[]> home1101() throws Exception {
-		logger.info("home1101");
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+	public String registerForm() {
+		logger.info("registerForm");
 
-		InputStream in = null;
-		ResponseEntity<byte[]> entity = null;
-
-		try {
-			HttpHeaders headers = new HttpHeaders();
-
-			in = new FileInputStream("C:\\Temp\\sample.png");
-
-			headers.setContentType(MediaType.IMAGE_PNG);
-
-			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
-		} catch (Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
-		} finally {
-			in.close();
-		}
-		return entity;
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/goHome1102", method = RequestMethod.GET)
-	public ResponseEntity<byte[]> home1102() throws Exception {
-		logger.info("home1102");
-
-		String fileName = "sample.png";
-
-		InputStream in = null;
-		ResponseEntity<byte[]> entity = null;
-
-		try {
-			HttpHeaders headers = new HttpHeaders();
-
-			in = new FileInputStream("C:\\Temp\\" + fileName);
-
-			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-			headers.add("Content-Disposition", "attachment; filename=\""
-					+ new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
-
-			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
-		} catch (Exception e) {
-			e.printStackTrace();
-			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
-		} finally {
-			in.close();
-		}
-
-		return entity;
+		return "registerForm";
 	}
 
 }
--- springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
@@ -4,9 +4,10 @@ import org.hdcd.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -18,125 +19,17 @@ public class MemberController {
 
     private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
-    @RequestMapping(value = "/registerForm", method = RequestMethod.GET)
-    public String registerForm() {
-        logger.info("registerForm");
+    @RequestMapping(value = "/uploadAjax", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
+    public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception {
+        String originalFilename = file.getOriginalFilename();
 
-        return "registerForm";
+        logger.info("originalName : " + originalFilename);
+
+        ResponseEntity<String> entity = new ResponseEntity<String>("UPLOAD SUCCESS" + originalFilename
+                , HttpStatus.OK);
+
+        return entity;
     }
 
-    @RequestMapping(value = "/registerFile01", method = RequestMethod.POST)
-    public String registerFile01(MultipartFile picture) throws Exception {
-        logger.info("registerFile01");
 
-        logger.info("originalName: " + picture.getOriginalFilename());
-        logger.info("size: " + picture.getSize());
-        logger.info("contentType: " + picture.getContentType());
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile02", method = RequestMethod.POST)
-    public String registerFile02(String userId, String password, MultipartFile picture) throws Exception {
-        logger.info("registerFile02");
-        logger.info("userId = " + userId);
-        logger.info("password = " + password);
-        logger.info("originalName: " + picture.getOriginalFilename());
-        logger.info("size: " + picture.getSize());
-        logger.info("contentType: " + picture.getContentType());
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile03", method = RequestMethod.POST)
-    public String registerFile03(Member member, MultipartFile picture) throws Exception {
-        logger.info("registerFile03");
-
-        logger.info("userId = " + member.getUserId());
-        logger.info("password = " + member.getPassword());
-
-        logger.info("originalName: " + picture.getOriginalFilename());
-        logger.info("size: " + picture.getSize());
-        logger.info("contentType: " + picture.getContentType());
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile04", method = RequestMethod.POST)
-    public String registerFile04(FileMember fileMember) throws Exception {
-        logger.info("registerFile04");
-
-        logger.info("userId = " + fileMember.getUserId());
-        logger.info("password = " + fileMember.getPassword());
-
-        MultipartFile picture = fileMember.getPicture();
-
-        logger.info("originalName: " + picture.getOriginalFilename());
-        logger.info("size: " + picture.getSize());
-        logger.info("contentType: " + picture.getContentType());
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile05", method = RequestMethod.POST)
-    public String registerFile05(MultipartFile picture, MultipartFile picture2) throws Exception {
-        logger.info("registerFile05");
-
-        logger.info("picture originalName: " + picture.getOriginalFilename());
-        logger.info("picture size: " + picture.getSize());
-        logger.info("picture contentType: " + picture.getContentType());
-
-        logger.info("picture2 originalName: " + picture2.getOriginalFilename());
-        logger.info("picture2 size: " + picture2.getSize());
-        logger.info("picture2 contentType: " + picture2.getContentType());
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile06", method = RequestMethod.POST)
-    public String registerFile06(List<MultipartFile> pictureList) throws Exception {
-        logger.info("registerFile06");
-
-        logger.info("registerFile06 pictureList.size() = " + pictureList.size());
-
-        for (MultipartFile picture : pictureList) {
-            logger.info("picture originalName: " + picture.getOriginalFilename());
-            logger.info("picture size: " + picture.getSize());
-            logger.info("picture contentType: " + picture.getContentType());
-        }
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile07", method = RequestMethod.POST)
-    public String registerFile07(MultiFileMember multiFileMember) throws Exception {
-        logger.info("registerFile07");
-
-        List<MultipartFile> pictureList = multiFileMember.getPictureList();
-
-        logger.info("registerFile07 pictureList.size() = " + pictureList.size());
-
-        for (MultipartFile picture : pictureList) {
-            logger.info("picture originalName: " +picture.getOriginalFilename());
-            logger.info("picture size: " + picture.getSize());
-            logger.info("picture contentType: " + picture.getContentType());
-        }
-
-        return "success";
-    }
-
-    @RequestMapping(value = "/registerFile08", method = RequestMethod.POST)
-    public String registerFile08(MultipartFile[] pictureList) throws Exception {
-        logger.info("registerFile08");
-
-        logger.info("registerFile08 pictureList.length = " + pictureList.length);
-
-        for (MultipartFile picture : pictureList) {
-            logger.info("picture originalName: " + picture.getOriginalFilename());
-            logger.info("picture size: " + picture.getSize());
-            logger.info("picture contentType: " + picture.getContentType());
-        }
-
-        return "success";
-    }
 }
