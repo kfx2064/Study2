83f6c8c476aa9dfc6c7926287029a3899d0bc397
kfx2064 <jth454@naver.com>
2023-10-27 17:02:34 +0900

    study spring boot.

--- springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
@@ -8,9 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class MemberController {
@@ -27,30 +25,52 @@ public class MemberController {
     @RequestMapping(value = "/registerForm01", method = RequestMethod.GET)
     public String registerForm01(Model model) {
         logger.info("registerForm01");
+
+        Map<String, String> hobbyMap = new HashMap<String, String>();
+        hobbyMap.put("01", "Sports");
+        hobbyMap.put("02", "Music");
+        hobbyMap.put("03", "Movie");
+
+        model.addAttribute("hobbyMap", hobbyMap);
+
         model.addAttribute("member", new Member());
-        return "registerForm";
+
+        return "registerForm01";
     }
 
     @RequestMapping(value = "/registerForm02", method = RequestMethod.GET)
     public String registerForm02(Model model) {
         logger.info("registerForm02");
 
-        Member member = new Member();
+        List<CodeLabelValue> hobbyCodeList = new ArrayList<CodeLabelValue>();
+        hobbyCodeList.add(new CodeLabelValue("01", "Sports"));
+        hobbyCodeList.add(new CodeLabelValue("02", "Music"));
+        hobbyCodeList.add(new CodeLabelValue("03", "Movie"));
 
-        String introduction = "안녕하세요\n반갑습니다.";
+        model.addAttribute("hobbyCodeList", hobbyCodeList);
 
-        member.setIntroduction(introduction);
+        model.addAttribute("member", new Member());
 
-        model.addAttribute("member", member);
-
-        return "registerForm";
+        return "registerForm02";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(Member member) {
+    public String register(Member member, Model model) {
         logger.info("register");
 
-        logger.info("member.getIntroduction() = " + member.getIntroduction());
+        List<String> hobbyList = member.getHobbyList();
+
+        if (hobbyList != null) {
+            logger.info("hobbyList != null = " + hobbyList);
+
+            for (int i = 0; i < hobbyList.size(); i++) {
+                logger.info("hobbyList(" + i + ") = " + hobbyList.get(i));
+            }
+        } else {
+            logger.info("hobbyList == null");
+        }
+
+        model.addAttribute("hobbyList", hobbyList);
 
         return "result";
     }
