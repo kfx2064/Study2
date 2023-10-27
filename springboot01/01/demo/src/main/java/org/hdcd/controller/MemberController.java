c2fc3c212c6fb3629227efb6c557a900c009ae09
kfx2064 <jth454@naver.com>
2023-10-27 16:09:16 +0900

    study spring boot.

--- springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/MemberController.java
@@ -24,10 +24,89 @@ public class MemberController {
         return "registerForm";
     }
 
-    @RequestMapping(value = "/result", method = RequestMethod.GET)
-    public String result() {
-        logger.info("result");
+    @RequestMapping(value = "/registerForm01", method = RequestMethod.GET)
+    public String registerForm01() {
+        logger.info("registerForm01");
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm02", method = RequestMethod.GET)
+    public String registerForm02(Model model) {
+        logger.info("registerForm02");
+
+        model.addAttribute("member", new Member());
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm03", method = RequestMethod.GET)
+    public String registerForm03(Model model) {
+        logger.info("registerForm03");
+
+        model.addAttribute("user", new Member());
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm04", method = RequestMethod.GET)
+    public String registerForm04(Model model) {
+        logger.info("registerForm04");
+
+        model.addAttribute("user", new Member());
+
+        return "registerForm2";
+    }
+
+    @RequestMapping(value = "/registerForm05", method = RequestMethod.GET)
+    public String registerForm05(Member member) {
+        logger.info("registerForm05");
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm06", method = RequestMethod.GET)
+    public String registerForm06(Member membere) {
+        logger.info("registerForm06");
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm07", method = RequestMethod.GET)
+    public String registerForm07(@ModelAttribute("user") Member member) {
+        logger.info("registerForm07");
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerForm08", method = RequestMethod.GET)
+    public String registerForm08(@ModelAttribute("user") Member member) {
+        logger.info("registerForm08");
+
+        return "registerForm2";
+    }
+
+    @RequestMapping(value = "/registerForm09", method = RequestMethod.GET)
+    public String registerForm09(Model model) {
+        logger.info("registerForm09");
+
+        Member member = new Member();
+
+        member.setUserId("hongkd");
+        member.setUserName("홍길동");
+
+        model.addAttribute("member", member);
+
+        return "registerForm";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(Member member) {
+        logger.info("register");
+
+        logger.info("member.getUserId() = " + member.getUserId());
+        logger.info("member.getUserName() = " + member.getUserName());
 
         return "result";
     }
+
 }
