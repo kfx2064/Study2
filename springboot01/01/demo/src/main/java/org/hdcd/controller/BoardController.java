2b58ec54801a5016e471db72e381becfcc6e9ecb
kfx2064 <jth454@naver.com>
2023-10-28 17:54:21 +0900

    study spring boot.

--- springboot01/01/demo/src/main/java/org/hdcd/controller/BoardController.java
+++ springboot01/01/demo/src/main/java/org/hdcd/controller/BoardController.java
@@ -1,15 +1,16 @@
 package org.hdcd.controller;
 
 import org.hdcd.domain.Board;
+import org.hdcd.service.BoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -19,58 +20,62 @@ public class BoardController {
 
     private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
+    @Autowired
+    private BoardService service;
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public void registerForm(Board board, Model model) throws Exception {
+        logger.info("registerForm");
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(Board board, Model model) throws Exception {
+        logger.info("register");
+
+        service.register(board);
+
+        model.addAttribute("msg", "등록이 완료되었습니다.");
+
+        return "board/success";
+    }
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String boardList() {
-        logger.info("boardList.");
-
-        return "board/list";
+    public void list(Model model) throws Exception {
+        logger.info("list");
+        model.addAttribute("list", service.list());
     }
 
-    @RequestMapping(value = "/{boardNo}", method = RequestMethod.GET, produces = "application/xml")
-    public ResponseEntity<Board> readToXml(@PathVariable("boardNo") int boardNo) {
-        logger.info("readToXml");
-
-        Board board = new Board();
-
-        board.setTitle("제목");
-        board.setContent("내용입니다.");
-        board.setWriter("홍길동");
-        board.setRegDate(new Date());
-
-        ResponseEntity<Board> entity = new ResponseEntity<Board>(board, HttpStatus.OK);
-
-        return entity;
-    }
-
-    @RequestMapping(value = "/{boardNo}", method = RequestMethod.GET
-            , consumes = "application/json")
-    public ResponseEntity<Board> readToJson(@PathVariable("boardNo") int boardNo) {
-        logger.info("readToJson");
-
-        Board board = new Board();
-        board.setTitle("제목");
-        board.setContent("내용입니다.");
-        board.setWriter("홍길동");
-        board.setRegDate(new Date());
-
-        ResponseEntity<Board> entity = new ResponseEntity<Board>(board, HttpStatus.OK);
-
-        return entity;
-    }
-
-    @RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
-    public ResponseEntity<Board> read(@PathVariable("boardNo") int boardNo) {
+    @RequestMapping(value = "/read", method = RequestMethod.GET)
+    public void read(@RequestParam("boardNo") int boardNo, Model model) throws Exception {
         logger.info("read");
+        model.addAttribute(service.read(boardNo));
+    }
 
-        Board board = new Board();
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String remove(@RequestParam("boardNo") int boardNo, Model model) throws Exception {
+        logger.info("remove");
 
-        board.setTitle("제목");
-        board.setContent("내용입니다.");
-        board.setWriter("홍길동");
-        board.setRegDate(new Date());
+        service.remove(boardNo);
 
-        ResponseEntity<Board> entity = new ResponseEntity<Board>(board, HttpStatus.OK);
+        model.addAttribute("msg", "삭제가 완료되었습니다.");
 
-        return entity;
+        return "board/success";
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.GET)
+    public void modifyForm(int boardNo, Model model) throws Exception {
+        logger.info("modifyForm");
+        model.addAttribute(service.read(boardNo));
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    public String modify(Board board, Model model) throws Exception {
+        logger.info("modify");
+
+        service.modify(board);
+
+        model.addAttribute("msg", "수정이 완료되었습니다.");
+
+        return "board/success";
     }
 }
