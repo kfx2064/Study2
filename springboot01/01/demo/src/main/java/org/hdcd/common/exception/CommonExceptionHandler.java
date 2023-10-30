ff7026f099ea6c4462906cf886b687489d8fa19a
kfx2064 <jth454@naver.com>
2023-10-30 17:31:18 +0900

    study spring boot.

---
+++ springboot01/01/demo/src/main/java/org/hdcd/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.hdcd.common.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(CommonExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public String handle(Exception e) {
+
+        logger.info(e.toString());
+
+        return "error/errorCommon";
+    }
+
+}
