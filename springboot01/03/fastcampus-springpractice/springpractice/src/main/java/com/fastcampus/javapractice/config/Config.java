1ae91297de623b3d2db441db9afcfa11e5d622ab
kfx2064 <jth454@naver.com>
2023-11-06 14:03:27 +0900

    practice.

--- springboot01/03/fastcampus-springpractice/springpractice/src/main/java/com/fastcampus/javapractice/config/Config.java
+++ springboot01/03/fastcampus-springpractice/springpractice/src/main/java/com/fastcampus/javapractice/config/Config.java
@@ -2,7 +2,9 @@ package com.fastcampus.javapractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fastcampus.javapractice")
 @Configuration
 public class Config {
---
+++ springboot01/03/fastcampus-springpractice/springpractice/src/main/java/com/fastcampus/javapractice/web/MyWebApplicationInitializer.java
@@ -0,0 +1,24 @@
+package com.fastcampus.javapractice.web;
+
+import com.fastcampus.javapractice.config.Config;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(Config.class);
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
