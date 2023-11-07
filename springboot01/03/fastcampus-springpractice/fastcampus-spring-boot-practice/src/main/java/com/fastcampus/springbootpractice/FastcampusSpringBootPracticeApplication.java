40a0178110bcadea8e3839afd22dd86d065929ee
kfx2064 <jth454@naver.com>
2023-11-07 11:58:46 +0900

    spring boot.

--- springboot01/03/fastcampus-springpractice/fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ springboot01/03/fastcampus-springpractice/fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,16 +1,49 @@
 package com.fastcampus.springbootpractice;
 
+import com.fastcampus.springbootpractice.properties.MyProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication(
-//        exclude = {WebMvcAutoConfiguration.class}
-)
+import javax.annotation.PostConstruct;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
+
+    private Integer height;
+    private Environment environment;
+    private ApplicationContext applicationContext;
+    private MyProperties myProperties;
+
+    public FastcampusSpringBootPracticeApplication(
+            @Value("${my.height}") Integer height
+            , Environment environment
+            , ApplicationContext applicationContext
+            , MyProperties myProperties
+    ) {
+        this.height = height;
+        this.environment = environment;
+        this.applicationContext = applicationContext;
+        this.myProperties = myProperties;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("[@Value] " + height);
+        System.out.println("[Environment] " + environment.getProperty("my.height"));
+        System.out.println("[ApplicationContext] " + applicationContext.getEnvironment().getProperty("my.height"));
+        System.out.println("[configurationProps] " + myProperties.getHeight());
     }
 
 }
