09f8b07786d87fb71b1ae2306e295fec846230fb
kfx2064 <jth454@naver.com>
2023-11-07 11:23:27 +0900

    spring boot.

--- springboot01/03/fastcampus-springpractice/fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ springboot01/03/fastcampus-springpractice/fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -2,8 +2,11 @@ package com.fastcampus.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+//        exclude = {WebMvcAutoConfiguration.class}
+)
 public class FastcampusSpringBootPracticeApplication {
 
     public static void main(String[] args) {
