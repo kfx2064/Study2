5e7e8dbdb30454d0638efada807a75ab8092e0d9
kfx2064 <jth454@naver.com>
2023-11-12 15:28:21 +0900

    training.

--- springboot01/03/springboot-developer/springboot-developer/src/main/resources/static/js/article.js
+++ springboot01/03/springboot-developer/springboot-developer/src/main/resources/static/js/article.js
@@ -34,6 +34,26 @@ if (modifyButton) {
         .then(() => {
             alert("수정이 완료되었습니다.");
             location.replace(`/articles/${id}`);
-        })
-    })
+        });
+    });
+}
+
+const createButton = document.getElementById("create-btn");
+
+if (createButton) {
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles", {
+            method: "POST"
+            , headers: {
+                "Content-Type": "application/json"
+            }
+            , body: JSON.stringify({
+                title: document.getElementById("title").value
+                , content: document.getElementById("content").value
+            })
+        }).then(() => {
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+        });
+    });
 }
