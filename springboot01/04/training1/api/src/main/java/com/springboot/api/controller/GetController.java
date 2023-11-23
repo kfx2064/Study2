196aafb713864d48e30535fbc1252eff48b48c65
kfx2064 <jth454@naver.com>
2023-11-23 17:21:16 +0900

    swagger.

--- springboot01/04/training1/api/src/main/java/com/springboot/api/controller/GetController.java
+++ springboot01/04/training1/api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -31,9 +32,9 @@ public class GetController {
 
     @GetMapping(value = "/request1")
     public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization
+            @ApiParam(value = "이름", required = true) @RequestParam String name,
+            @ApiParam(value = "이메일", required = true) @RequestParam String email,
+            @ApiParam(value = "회사", required = true) @RequestParam String organization
     ) {
         return name + " " + email + " " + organization;
     }
