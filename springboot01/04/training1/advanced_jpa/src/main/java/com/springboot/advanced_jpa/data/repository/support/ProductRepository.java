56793ef0814e18ea90b2a44a02adb926159fc4f7
kfx2064 <jth454@naver.com>
2023-11-24 16:31:16 +0900

    test.

---
+++ springboot01/04/training1/advanced_jpa/src/main/java/com/springboot/advanced_jpa/data/repository/support/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.advanced_jpa.data.repository.support;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("productRepositorySupport")
+public interface ProductRepository extends JpaRepository<Product, Long>
+        , ProductRepositoryCustom {
+
+}
---
+++ springboot01/04/training1/advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/support/ProductRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.springboot.advanced_jpa.data.repository.support;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import com.springboot.advanced_jpa.data.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import java.util.List;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void findByNameTest() {
+        List<Product> productList = productRepository.findByName("펜");
+
+        for (Product product : productList) {
+            System.out.println(product.getNumber());
+            System.out.println(product.getName());
+            System.out.println(product.getPrice());
+            System.out.println(product.getStock());
+        }
+    }
+
+}
