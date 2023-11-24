e832ab12ad2fcdd0ab4f6043398c4cce648731ca
kfx2064 <jth454@naver.com>
2023-11-24 18:17:56 +0900

    jpa.

--- springboot01/04/training1/relationship/src/main/java/com/springboot/relationship/data/entity/Producer.java
+++ springboot01/04/training1/relationship/src/main/java/com/springboot/relationship/data/entity/Producer.java
@@ -12,17 +12,22 @@ import java.util.List;
 @NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-@Table(name = "provider")
-public class Provider extends BaseEntity {
+@Table(name = "producer")
+public class Producer extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String code;
+
     private String name;
 
-    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST)
+    @ManyToMany
     @ToString.Exclude
-    private List<Product> productList = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
 
+    public void addProduct(Product product) {
+        products.add(product);
+    }
 }
--- springboot01/04/training1/relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
+++ springboot01/04/training1/relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
@@ -2,6 +2,7 @@ package com.springboot.relationship.data.repository;
 
 import com.springboot.relationship.data.entity.Product;
 import com.springboot.relationship.data.entity.Provider;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,39 @@ public class ProviderRepositoryTest {
 
     @Autowired
     ProviderRepository providerRepository;
+
+    @Test
+    void cascadeTest() {
+        Provider provider = savedProvider("새로운 공급업체");
+
+        Product product1 = savedProduct("상품1", 1000, 1000);
+        Product product2 = savedProduct("상품2", 500, 1500);
+        Product product3 = savedProduct("상품3", 750, 500);
+
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(product1, product2, product3));
+
+        providerRepository.save(provider);
+    }
+
+    private Provider savedProvider(String name) {
+        Provider provider = new Provider();
+        provider.setName(name);
+
+        return provider;
+    }
+
+    private Product savedProduct(String name, Integer price, Integer stock) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+
+        return product;
+    }
 
     @Test
     void relationshipTest() {
@@ -49,9 +83,11 @@ public class ProviderRepositoryTest {
         List<Product> products = providerRepository.findById(provider.getId()).get()
                 .getProductList();
 
-        for (Product product : products) {
+        //System.out.println("products : " + products.toString());
+
+        /*for (Product product : products) {
             System.out.println(product);
-        }
+        }*/
 
     }
 
@@ -72,11 +108,11 @@ public class ProviderRepositoryTest {
         productRepository.save(product);
 
         // 테스트
-        System.out.println(
+        /*System.out.println(
             "product : " + productRepository.findById(1L)
                     .orElseThrow(RuntimeException::new));
 
         System.out.println("provider : " + productRepository.findById(1L)
-                .orElseThrow(RuntimeException::new).getProvider());
+                .orElseThrow(RuntimeException::new).getProvider());*/
     }
 }
