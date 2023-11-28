62340182fe8849ec0e9e82a5e19717534ca25654
kfx2064 <jth454@naver.com>
2023-11-28 11:02:32 +0900

    test.

---
+++ springboot01/04/training2/advanced_jpa/src/main/java/com/springboot/advanced_jpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.springboot.advanced_jpa.service.impl;
+
+import com.springboot.advanced_jpa.data.dto.ProductDto;
+import com.springboot.advanced_jpa.data.dto.ProductResponseDto;
+import com.springboot.advanced_jpa.data.repository.ProductRepository;
+import com.springboot.advanced_jpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductResponseDto getProduct(Long number) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto saveProduct(ProductDto productDto) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto changeProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+
+    }
+}
