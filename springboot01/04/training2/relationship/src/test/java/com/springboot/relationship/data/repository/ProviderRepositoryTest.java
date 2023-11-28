cf87d9ba0617792db3ef4ebbff45818a06fc5162
kfx2064 <jth454@naver.com>
2023-11-28 14:10:27 +0900

    test

---
+++ springboot01/04/training2/relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.springboot.relationship.data.repository;
+
+import com.springboot.relationship.data.entity.Product;
+import com.springboot.relationship.data.entity.Provider;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProviderRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ProviderRepository providerRepository;
+
+    @Test
+    void relationshipTest1() {
+        Provider provider = new Provider();
+        provider.setName("ㅇㅇ물산");
+
+        providerRepository.save(provider);
+
+        Product product = new Product();
+        product.setName("가위");
+        product.setPrice(5000);
+        product.setStock(500);
+        product.setProvider(provider);
+
+        productRepository.save(product);
+
+        System.out.println(
+                "product : " + productRepository.findById(1L)
+                        .orElseThrow(RuntimeException::new)
+        );
+
+        System.out.println("provider : " + productRepository.findById(1L)
+                .orElseThrow(RuntimeException::new).getProvider());
+    }
+
+}
