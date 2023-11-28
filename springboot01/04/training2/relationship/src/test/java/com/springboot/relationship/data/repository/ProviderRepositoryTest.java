bb6c8d67e43b428c6276e128be5b150f54b74c3e
kfx2064 <jth454@naver.com>
2023-11-28 15:10:09 +0900

    test

--- springboot01/04/training2/relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
+++ springboot01/04/training2/relationship/src/test/java/com/springboot/relationship/data/repository/ProviderRepositoryTest.java
@@ -2,6 +2,7 @@ package com.springboot.relationship.data.repository;
 
 import com.springboot.relationship.data.entity.Product;
 import com.springboot.relationship.data.entity.Provider;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,40 @@ public class ProviderRepositoryTest {
 
     @Autowired
     ProviderRepository providerRepository;
+
+    @Test
+    void cascadeTest() {
+        Provider provider = savedProvier("새로운 공급업체");
+
+        Product product1 = savedProduct("상품1", 1000, 1000);
+        Product product2 = savedProduct("상품2", 500, 1500);
+        Product product3 = savedProduct("상품3", 750, 500);
+
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(
+                product1, product2, product3
+        ));
+
+        providerRepository.save(provider);
+    }
+
+    private Provider savedProvier(String name) {
+        Provider provider = new Provider();
+        provider.setName(name);
+        return provider;
+    }
+
+    private Product savedProduct(String name, Integer price, Integer stock) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+
+        return product;
+    }
 
     @Test
     void relationshipTest() {
