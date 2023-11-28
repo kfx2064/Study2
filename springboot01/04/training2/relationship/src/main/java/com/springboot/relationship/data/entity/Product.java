c95fd2e0a4f3d6a8bcf75788cb134db08f3a34de
kfx2064 <jth454@naver.com>
2023-11-28 15:01:09 +0900

    test

--- springboot01/04/training2/relationship/src/main/java/com/springboot/relationship/data/entity/Product.java
+++ springboot01/04/training2/relationship/src/main/java/com/springboot/relationship/data/entity/Product.java
@@ -3,6 +3,8 @@ package com.springboot.relationship.data.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -35,5 +37,13 @@ public class Product extends BaseEntity {
     @JoinColumn(name = "provider_id")
     @ToString.Exclude
     private Provider provider;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Producer> producers = new ArrayList<>();
+
+    public void addProducer(Producer producer) {
+        this.producers.add(producer);
+    }
 
 }
