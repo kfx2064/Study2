b9ccf5da8503e8463b255af4b3456240cbbab23f
kfx2064 <jth454@naver.com>
2023-11-29 09:48:22 +0900

    webclient.

--- springboot01/04/training2/rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
+++ springboot01/04/training2/rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -1,13 +1,18 @@
 package com.springboot.rest.service;
 
 import com.springboot.rest.dto.MemberDto;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 
 @Service
 public class RestTemplateService {
@@ -102,5 +107,27 @@ public class RestTemplateService {
                 MemberDto.class);
 
         return responseEntity;
+    }
+
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        CloseableHttpClient client = HttpClientBuilder.create()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate;
     }
 }
