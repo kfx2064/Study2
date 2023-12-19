f83e1fa181fdac3f87fcbbed747c7377aa2d031d
kfx2064 <jth454@naver.com>
2023-12-19 13:56:24 +0900

    vue.

---
+++ Vue07/03/Vue4/07/test-vue-app/src/components/CheckboxItem2.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <input type="checkbox" :checked="idol.checked" /> {{ idol.name }}
+    </li>
+</template>
+
+<script>
+    export default {
+        name : "CheckboxIem2",
+        props : ["idol"],
+    }
+</script>
+
+<style scoped>
+
+</style>
