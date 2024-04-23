265287ac9127efe75e36147a1499dd7c51382419
jeongtaehyun <jth454@naver.com>
2024-04-23 21:41:16 +0900

    1

---
+++ Java11/01/01/src/first/TestMain2.java
@@ -0,0 +1,46 @@
+package first;
+
+public class TestMain2 {
+
+	public static void main(String[] args) {
+	
+		TestMain2 test = new TestMain2();
+		
+		Math add = (int x, int y) -> x + y;
+		Math substract = (x, y) -> x - y;
+		Math multiply = (int x, int y) -> { return x * y; };
+		Math divide = (int x, int y) -> x / y;
+		
+		System.out.println("20 + 5 = " + test.operation(20, 5, add));
+		System.out.println("20 - 5 = " + test.operation(20, 5, substract));
+		System.out.println("20 * 5 = " + test.operation(20, 5, multiply));
+		System.out.println("20 / 5 = " + test.operation(20, 5, divide));
+		
+		Echo msg1 = (msg) -> { System.out.println("Hello! " + msg); };
+		Echo msg2 = msg -> System.out.println("Hello! " + msg);
+		
+		msg1.echo("a");
+		msg2.echo("b");
+		
+		Process process = () -> System.out.println("no parameter!");
+		process.echo();
+		
+	}
+	
+	private int operation(int x, int y, Math math) {
+		return math.operation(x, y);
+	}
+	
+	interface Math {
+		int operation(int x, int y);
+	}
+	
+	interface Echo {
+		void echo(String msg);
+	}
+	
+	interface Process {
+		void echo();
+	}
+	
+}
