81cff28c84f724154cc9383c31e9eb26e8d235d6
jeongtaehyun <jth454@naver.com>
2024-08-02 20:44:55 +0900

    form and error.

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.answer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,16 @@ public class AnswerController {
 	
 	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id,
-			@RequestParam(value = "content") String content) {
+			AnswerForm answerForm, BindingResult bindingResult) {
+		
 		Question question = this.questionService.getQuestion(id);
-		this.answerService.create(question, content);
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
 
