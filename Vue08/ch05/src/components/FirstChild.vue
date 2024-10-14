e4688deb72ce91b8af153952036173742560c65f
kfx2064 <jth454@naver.com>
2024-10-14 17:29:56 +0900

    study.

---
+++ Vue08/ch05/src/components/FirstChild.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            num: 5,
+        };
+    },
+    computed: {
+        doubleNum() {
+            return this.num * 2;
+        },
+    },
+    methods: {
+        increment() {
+            this.num++;
+        },
+    },
+}
+</script>
+<template>
+    <h1>{{ num }}</h1>
+</template>
+<style>
+    h1 {
+        color: blue;
+    }
+    p {
+        color: green;
+    }
+</style>
---
+++ Vue08/ch05/src/components/UserProfile.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    methods: {
+        parentEventCall() {
+            this.$emit('print-hello', { name: '철수', age: 30 });
+        },
+    },
+}
+</script>
+<template>
+    <button @click="parentEventCall">클릭</button>
+</template>
