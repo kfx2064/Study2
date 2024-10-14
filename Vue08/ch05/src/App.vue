174c5ea1980142f7da40dc4b37b6490618da3b3c
kfx2064 <jth454@naver.com>
2024-10-14 18:03:10 +0900

    vue component.

--- Vue08/ch05/src/App.vue
+++ Vue08/ch05/src/App.vue
@@ -1,19 +1,14 @@
 <script>
-import FirstChild from '@/components/FirstChild.vue';
+import CustomButton from '@/components/CustomButton.vue';
 export default {
   components: {
-    FirstChild,
+    CustomButton,
   },
-  mounted() {
-    console.log(this.$refs.childRef.childNum);
-    console.log(this.$refs.childRef.childNumOddAdd);
-    console.log(this.$refs.childRef.getChildNum());
-  }
 }
 </script>
 
 <template>
-  <FirstChild ref="childRef" />
+  <CustomButton />
 </template>
 
 <style scoped>
