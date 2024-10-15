6a4902a9e3fccd282f8701a3ccdbf0a379832d8c
kfx2064 <jth454@naver.com>
2024-10-15 11:05:07 +0900

    composition props.

---
+++ Vue08/ch07_3_2/src/components/DefineProps.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    count: Number,
+    state: [Object, Array],
+});
+</script>
+
+<template>
+    <h1>{{ props.count }}</h1>
+    <h1>{{ props.state.count }}</h1>
+</template>
