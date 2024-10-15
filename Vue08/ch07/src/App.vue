80673e99cbac373872df63058f35204ea90ff464
kfx2064 <jth454@naver.com>
2024-10-15 10:48:19 +0900

    composition watch.

--- Vue08/ch07/src/App.vue
+++ Vue08/ch07/src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-import RefEx2 from '@/components/RefEx2.vue';
-import ComputedEx1 from '@/components/ComputedEx1.vue';
-import ComputedEx2 from '@/components/ComputedEx2.vue';
-import ComputedEx3 from '@/components/ComputedEx3.vue';
+import WatchPostEffectEx1 from '@/components/WatchPostEffectEx1.vue';
 </script>
 
 <template>
-  <ComputedEx3 />
+  <WatchPostEffectEx1 />
   <br />
 </template>
   
