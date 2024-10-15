f4b8093127e839955d1fbb521bac395325fa4e13
kfx2064 <jth454@naver.com>
2024-10-15 10:13:16 +0900

    composition ref.

--- Vue08/ch07/src/App.vue
+++ Vue08/ch07/src/App.vue
@@ -2,11 +2,13 @@
 import DataEx1 from '@/components/DataEx1.vue';
 import DataEx2 from '@/components/DataEx2.vue';
 import DataEx3 from '@/components/DataEx3.vue';
+import RefEx1 from '@/components/RefEx1.vue';
+import RefEx2 from '@/components/RefEx2.vue';
 </script>
 
 <template>
-  <DataEx3 />
-  <br/>
+  <RefEx2 />
+  <br />
 </template>
   
 <style>
