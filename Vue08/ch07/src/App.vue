a73b2ecce1273f19ce504792c9a981b29e9501c9
kfx2064 <jth454@naver.com>
2024-10-15 10:25:22 +0900

    composition computed.

--- Vue08/ch07/src/App.vue
+++ Vue08/ch07/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import DataEx1 from '@/components/DataEx1.vue';
-import DataEx2 from '@/components/DataEx2.vue';
-import DataEx3 from '@/components/DataEx3.vue';
-import RefEx1 from '@/components/RefEx1.vue';
 import RefEx2 from '@/components/RefEx2.vue';
+import ComputedEx1 from '@/components/ComputedEx1.vue';
+import ComputedEx2 from '@/components/ComputedEx2.vue';
+import ComputedEx3 from '@/components/ComputedEx3.vue';
 </script>
 
 <template>
-  <RefEx2 />
+  <ComputedEx3 />
   <br />
 </template>
   
