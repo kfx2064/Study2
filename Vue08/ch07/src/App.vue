e110152542178668cbe2588abcac84f3025749ab
kfx2064 <jth454@naver.com>
2024-10-15 09:47:51 +0900

    composition.

---
+++ Vue08/ch07/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DataEx1 from '@/components/DataEx1.vue';
+import DataEx2 from '@/components/DataEx2.vue';
+import DataEx3 from '@/components/DataEx3.vue';
+</script>
+
+<template>
+  <DataEx3 />
+  <br/>
+</template>
+  
+<style>
+
+</style>
---
+++ Vue08/ch07/src/components/DataEx2.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+const arr1 = ref([10]);
+const arr2 = ref([10]);
+setTimeout(() => {
+    arr1.value = [10, 20];
+});
+setTimeout(() => {
+    arr2.push(20);
+});
+</script>
+
+<template>
+    <h1>arr1 : {{ arr1 }}</h1>
+    <h2>arr2 : {{ arr2 }}</h2>
+</template>
