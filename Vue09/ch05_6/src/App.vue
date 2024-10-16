9c523923c7f4dd9784e75c76eaac6d3cee9a024e
kfx2064 <jth454@naver.com>
2024-10-16 12:54:08 +0900

    slot.

---
+++ Vue09/ch05_6/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import CustomButton from './components/CustomButton.vue';
+import DefaultLayout from './components/DefaultLayout.vue';
+export default {
+  components: {
+    CustomButton,
+    DefaultLayout,
+  },
+}
+</script>
+
+<template>
+  <DefaultLayout>
+    <template v-slot:header>
+      <header>
+        <h1>Header</h1>
+      </header>
+    </template>
+    <template v-slot:main>
+      <main>
+        <h1>Main</h1>
+      </main>
+    </template>
+    <template v-slot:footer>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </template>
+  </DefaultLayout>
+</template>
+
+<style>
+
+</style>
