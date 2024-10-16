71950380453b3129f71627fc4cf85dfca7cf9780
kfx2064 <jth454@naver.com>
2024-10-16 12:45:51 +0900

    child parent.

---
+++ Vue09/ch05_5/src/components/FirstChild.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    mounted() {
+        console.log(this.$parent.parentNum);
+        console.log(this.$parent.numOddEvent);
+        console.log(this.$parent.getParentNum());
+    },
+}
+</script>
+
+<template>
+    <h1>ParentChild</h1>
+</template>
