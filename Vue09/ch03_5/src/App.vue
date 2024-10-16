bf613eaf8b97730412cc583e5eaaa6cf5529208f
kfx2064 <jth454@naver.com>
2024-10-16 11:27:24 +0900

    event.

---
+++ Vue09/ch03_5/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      selected: 'banana',
+      price: 500,
+    };
+  },
+  methods: {
+    onKeyupHandler() {
+      console.log(`keyup event!`);
+    },
+  },
+}
+</script>
+  
+<template>
+  <input type="text" @keyup="onKeyupHandler" />
+</template>
+
+<style scoped>
+
+</style>
