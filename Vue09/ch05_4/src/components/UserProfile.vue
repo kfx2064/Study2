94f7257331753c85ee182ad6c577ec84bfaca5a6
kfx2064 <jth454@naver.com>
2024-10-16 12:34:35 +0900

    parent, child.

---
+++ Vue09/ch05_4/src/components/UserProfile.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    methods: {
+        parentEventCall() {
+            this.$emit('print-hello', '철수', 30);
+        },
+    },
+}
+</script>
+
+<template>
+    <button @click="parentEventCall">클릭</button>
+</template>
