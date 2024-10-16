1aaba55f67129bd87fb32b62e5b58c2a455c055f
kfx2064 <jth454@naver.com>
2024-10-16 12:24:02 +0900

    refs.

---
+++ Vue09/ch05_2/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import FirstChild from './components/FirstChild.vue';
+export default {
+  components: {
+    FirstChild,
+  },
+  mounted() {
+    const childNum = this.$refs.child.num;
+    const childDoubleNum = this.$refs.child.doubleNum;
+    console.log(`메서드 호출 전: ${childNum}`);
+    console.log(`메서드 호출 전: ${childDoubleNum}`);
+  
+    this.$refs.child.increment();
+    console.log(`메서드 호출 전: ${childNum}`);
+    console.log(`메서드 호출 전: ${childDoubleNum}`);
+  
+  },
+}
+</script>
+
+<template>
+  <FirstChild ref="child" />
+</template>
+
+<style scoped>
+
+</style>
