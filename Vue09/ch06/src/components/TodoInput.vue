c1ec79bef95f2b33fc65550ff6a74049d3d6449d
kfx2064 <jth454@naver.com>
2024-10-16 14:02:00 +0900

    todo.

---
+++ Vue09/ch06/src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+            inputMsg: '',
+        };
+    },
+    emits: [
+        "add-todo",
+    ],
+    methods: {
+        addTodo() {
+            console.log(this.inputMsg);
+            this.$emit('add-todo', this.inputMsg);
+            this.inputMsg = "";
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="todo__input">
+        <input 
+            type="text" 
+            class="todo__input-text" 
+            placeholder="할 일을 입력하세요."
+            v-model="inputMsg" 
+            @keydown.enter="addTodo" />
+        <button class="todo__input-btn" @click="addTodo">등록</button>
+    </div>
+</template>
+
+<style></style>
