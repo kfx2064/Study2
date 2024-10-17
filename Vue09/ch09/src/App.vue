c8d12773cc8d0d6342e2a72c721c94c37d8a3c27
kfx2064 <jth454@naver.com>
2024-10-17 10:53:32 +0900

    stop.

---
+++ Vue09/ch09/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import ChildComp from './components/ChildComp.vue';
+const num = ref(0);
+const update = () => {
+  num.value++;
+};
+</script>
+
+<template>
+  <ChildComp :num="num" @update="update" />
+</template>
+
+<style scoped>
+
+</style>
