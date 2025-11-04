b8a482a4e43dc56aba6091ca8410f2c4656559af
jeongtaehyun <jth454@naver.com>
2025-11-04 20:16:58 +0900

    Lambda, Optional, Stream 연습 중.

---
+++ 새로운자바기술/java_ex02/etc3/src/ex11/ToParallelStream.java
@@ -0,0 +1,24 @@
+package ex11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+public class ToParallelStream {
+    public static void main(String[] args) {
+        List<String> ls = Arrays.asList("Box", "Simple", "Complex", "Robot");
+        Stream<String> ss = ls.stream();
+
+        BinaryOperator<String> lc = (s1, s2) -> {
+            if (s1.length() > s2.length())
+                return s1;
+            else
+                return s2;
+        };
+
+        String str = ss.parallel()
+                .reduce("", lc);
+        System.out.println(str);
+    }
+}
