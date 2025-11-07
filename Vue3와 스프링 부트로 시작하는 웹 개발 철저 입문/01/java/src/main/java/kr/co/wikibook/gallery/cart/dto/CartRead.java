24bfed276602e5f84e9260916f573c61e367fb16
jeongtaehyun <jth454@naver.com>
2025-11-07 18:43:57 +0900

    spring, vue 학습.

---
+++ Vue3와 스프링 부트로 시작하는 웹 개발 철저 입문/01/java/src/main/java/kr/co/wikibook/gallery/cart/dto/CartRead.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.gallery.cart.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CartRead {
+
+    private Integer id;
+    private Integer itemId;
+
+}
