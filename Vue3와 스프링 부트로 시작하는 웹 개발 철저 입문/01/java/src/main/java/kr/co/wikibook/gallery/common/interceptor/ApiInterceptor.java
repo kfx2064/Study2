7204c7a54e965ea2f6cb8b5f306087eb446dfec8
jeongtaehyun <jth454@naver.com>
2025-11-08 16:03:56 +0900

    spring, vue 학습.

--- Vue3와 스프링 부트로 시작하는 웹 개발 철저 입문/01/java/src/main/java/kr/co/wikibook/gallery/common/interceptor/ApiInterceptor.java
+++ Vue3와 스프링 부트로 시작하는 웹 개발 철저 입문/01/java/src/main/java/kr/co/wikibook/gallery/common/interceptor/ApiInterceptor.java
@@ -1,2 +1,26 @@
-package kr.co.wikibook.gallery.common.interceptor;public class ApiInterceptor {
+package kr.co.wikibook.gallery.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.wikibook.gallery.account.helper.AccountHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class ApiInterceptor implements HandlerInterceptor {
+
+    private final AccountHelper accountHelper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+        // 로그인 회원 아이디가 없으면
+        if (accountHelper.getMemberId(req) == null) {
+            res.setStatus(401);
+            return false;
+        }
+
+        return true;
+    }
 }
