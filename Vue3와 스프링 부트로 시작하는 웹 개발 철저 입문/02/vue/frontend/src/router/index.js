0053258d26c4030064e2ddd5f2e31afe3a6c6ae1
jeongtaehyun <jth454@naver.com>
2025-11-09 15:24:53 +0900

    vue study.

--- Vue3와 스프링 부트로 시작하는 웹 개발 철저 입문/02/vue/frontend/src/router/index.js
+++ Vue3와 스프링 부트로 시작하는 웹 개발 철저 입문/02/vue/frontend/src/router/index.js
@@ -8,6 +8,16 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/Home.vue'),
     },
+      {
+          path: '/join',
+          name: 'join',
+          component: () => import('../views/Join.vue'),
+      },
+      {
+          path: '/login',
+          name: 'login',
+          component: () => import('../views/Login.vue'),
+      },
   ]
 });
 
