522c6604fd3a0c27287c2bbea4a0d43e7b33a659
jeongtaehyun <jth454@naver.com>
2025-11-13 17:09:29 +0900

    vue study.

---
+++ Vue 입문 강의/ex11/vue/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  methods: {
+    dataEvent() {
+      alert("이벤트 발생");
+    },
+    eventClick(event) {
+      console.log(event);
+    }
+  },
+}
+</script>
+
+<template>
+  <button type="button" v-on:click="dataEvent">클릭1</button>
+  <button type="button" @click="dataEvent">클릭2</button>
+  <button type="button" @dblclick="dataEvent">클릭3</button>
+  <button type="button" @click="eventClick($event)">클릭4</button>
+</template>
---
+++ Vue 입문 강의/ex12/vue/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  methods: {
+    onKeyEvent() {
+      alert("이벤트 발생");
+    },
+  },
+}
+</script>
+
+<template>
+  <input type="text" @keyup.enter="onKeyEvent" />
+  <input type="text" @keyup.tab="onKeyEvent" />
+  <input type="text" @keyup.delete="onKeyEvent" />
+  <input type="text" @keyup.esc="onKeyEvent" />
+
+  <input type="text" @keyup.space="onKeyEvent" />
+  <input type="text" @keyup.up="onKeyEvent" />
+  <input type="text" @keyup.down="onKeyEvent" />
+
+  <input type="text" @keyup.left="onKeyEvent" />
+  <input type="text" @keyup.right="onKeyEvent" />
+
+  <button type="button" @click.left="onKeyEvent">Left</button>
+  <button type="button" @click.right="onKeyEvent">Right</button>
+  <button type="button" @click.middle="onKeyEvent">Middle</button>
+</template>
