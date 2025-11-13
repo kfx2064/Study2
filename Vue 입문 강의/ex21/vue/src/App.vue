d9a36d13db9588fe8d35ba70f53b38f979f0a67b
jeongtaehyun <jth454@naver.com>
2025-11-13 20:21:11 +0900

    vue

---
+++ Vue 입문 강의/ex21/vue/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  methods: {
+    keyupEvent() {
+      console.log('키업 이벤트');
+    },
+  },
+}
+</script>
+
+<template>
+  <input type="text" @keyup="keyupEvent" />
+</template>
