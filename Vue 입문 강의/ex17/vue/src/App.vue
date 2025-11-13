a67e6b89ef96c434ae91cf5d571b7b9a601b9074
jeongtaehyun <jth454@naver.com>
2025-11-13 20:08:03 +0900

    vue study.

---
+++ Vue 입문 강의/ex17/vue/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      team: 'alpha',
+    };
+  },
+  methods: {
+    showData() {
+      console.log(this.team);
+    },
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <input type="radio" id="alpha" name="alpha" value="alpha" v-model="team" />alpha
+    <input type="radio" id="bravo" name="bravo" value="bravo" v-model="team" />bravo
+    <button type="button" @click="showData">클릭</button>
+  </form>
+</template>
---
+++ Vue 입문 강의/ex18/vue/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      getTeam: 'charlie',
+    };
+  },
+  methods: {
+    showData() {
+      console.log(this.getTeam);
+    },
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <select v-model="getTeam">
+      <option value="alpha">alpha</option>
+      <option value="bravo">bravo</option>
+      <option value="charlie">charlie</option>
+    </select>
+    <button type="button" @click="showData">클릭</button>
+  </form>
+</template>
