39f6797b8ac68777d5e7b4ca342807777a62a035
jeongtaehyun <jth454@naver.com>
2025-11-13 19:43:37 +0900

    vue study.

---
+++ Vue 입문 강의/ex13/vue/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      userid: '',
+      //userid: 'jth',
+      userpw: '',
+    };
+  },
+  methods: {
+    check() {
+      console.log(`id: ${this.userpw}, pw: ${this.userpw}`);
+    },
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <label for="userid">
+      아이디 : <input type="text" id="userid" v-model="userid" />
+    </label>
+    <label for="userpw">
+      비밀번호 : <input type="text" id="userpw" v-model="userpw" />
+    </label>
+    <button type="button" @click="check">로그인</button>
+  </form>
+</template>
---
+++ Vue 입문 강의/ex14/vue/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      data: '',
+    };
+  },
+  methods: {
+    showData() {
+      console.log(`Good day : ${this.data}`);
+    },
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <textarea v-model="data"></textarea>
+    <button type="button" @click="showData">클릭</button>
+  </form>
+</template>
