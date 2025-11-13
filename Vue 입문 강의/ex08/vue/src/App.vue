7a55fe17aebdc0fec99f73de3fba0c45da4ea8a9
jeongtaehyun <jth454@naver.com>
2025-11-13 16:16:34 +0900

    vue study

---
+++ Vue 입문 강의/ex08/vue/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      part: "alpha",
+      //part: "bravo",
+    };
+  },
+};
+</script>
+
+<template>
+  <p v-if="part === 'alpha'">파트는 alpha.</p>
+  <p v-if="part === 'bravo'">파트는 bravo.</p>
+  <p v-if="part === 'charlie'">파트는 charlie.</p>
+</template>
---
+++ Vue 입문 강의/ex09/vue/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      part: true,
+    };
+  },
+}
+</script>
+
+<template>
+  <p v-show="part">파트가 보입니다.</p>
+  <p v-show="!part">파트가 안 보입니다.</p>
+</template>
