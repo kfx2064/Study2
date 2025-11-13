8b014606568b961323a21e12be42c0ca8d9b108f
jeongtaehyun <jth454@naver.com>
2025-11-13 20:18:37 +0900

    vue

---
+++ Vue 입문 강의/ex19/vue/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      team: '',
+      write: '',
+      part: 'sales',
+      unit: 5,
+    };
+  },
+  methods: {
+    changeEvent($event) {
+      this.write = $event.target.value;
+    },
+    selectEvent($event) {
+      if (this.part === 'sales') {
+        this.unit = 5;
+      }
+      if (this.part === 'tech') {
+        this.unit = 7;
+      }
+    },
+  }
+}
+</script>
+
+<template>
+  <input type="text" v-model="team">
+  {{ team }}
+  <br>
+  <input type="text" @input="changeEvent($event)" />
+  {{ write }}
+  <br>
+  <select v-model="part" @change="selectEvent">
+    <option value="sales">영업부</option>
+    <option value="tech">기술부</option>
+  </select>
+  인원 : {{ unit }}
+</template>
---
+++ Vue 입문 강의/ex20/vue/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      team: 'sales',
+      unit: 5,
+      location: 'seoul',
+    };
+  },
+  methods: {
+    submitEvent(e) {
+      e.preventDefault();
+      console.log("team : " + this.team);
+      console.log("unit : " + this.unit);
+      console.log("location : " + this.location);
+    },
+  },
+}
+</script>
+
+<template>
+  <form @submit="submitEvent">
+    <input type="text" id="team" name="team" v-model="team" />
+    <input type="text" id="unit" name="unit" v-model="unit" />
+    <input type="text" id="location" name="location" v-model="location" />
+    <button type="submit">전송</button>
+  </form>
+</template>
