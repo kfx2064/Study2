eb64fa16743fe04f38591878621193e7af0f9d88
jeongtaehyun <jth454@naver.com>
2025-11-13 16:02:51 +0900

    vue 강의

---
+++ Vue 입문 강의/ex04/vue/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "james",
+      gender: "male",
+    };
+  },
+}
+</script>
+
+<template>
+  <p>나의 이름은 {{ name }}</p>
+  <p>나의 성별은 {{ gender }}</p>
+</template>
---
+++ Vue 입문 강의/ex05/vue/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      contents: "<h1>Hello, world!</h1>"
+    };
+  },
+}
+</script>
+
+<template>
+  <div>{{ contents }}</div>
+  <div v-html="contents"></div>
+  <div v-text="contents"></div>
+  <div v-pre>{{ contents }}</div>
+</template>
