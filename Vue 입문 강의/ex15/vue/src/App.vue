059c82073677d5b57a07a377ad3fcd91dfe55318
jeongtaehyun <jth454@naver.com>
2025-11-13 20:01:30 +0900

    vue study.

---
+++ Vue 입문 강의/ex15/vue/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      alpha: true,
+      bravo: false,
+    };
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <label for="first">
+      <input type="checkbox" id="first" v-model="alpha" value="first" />First
+    </label>
+    <label for="second">
+      <input type="checkbox" id="second" v-model="bravo" value="second" />Second
+    </label>
+  </form>
+</template>
---
+++ Vue 입문 강의/ex16/vue/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      team: ['alpha', 'bravo'],
+    };
+  },
+  methods: {
+    showData() {
+      console.log(this.team);
+    },
+  },
+}
+</script>
+
+<template>
+  <form id="loginFrm">
+    <input type="checkbox" id="team1" value="alpha" v-model="team" />알파
+    <input type="checkbox" id="team1" value="bravo" v-model="team" />브라보
+    <input type="checkbox" id="team1" value="charlie" v-model="team" />찰리
+    <button type="button" @click="showData">클릭</button>
+  </form>
+</template>
