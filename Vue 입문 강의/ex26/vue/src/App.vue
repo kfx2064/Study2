5472c2b28879aacba0011e7110797acfa65637b4
jeongtaehyun <jth454@naver.com>
2025-11-14 12:16:02 +0900

    vue

---
+++ Vue 입문 강의/ex26/vue/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  methods: {
+    onClick1() {
+      const param1 = this.$refs.param1;
+      const cnt1 = param1.innerText;
+      console.log(cnt1);
+    },
+    onClick2() {
+      const param2 = this.$refs.param2;
+      const cnt2 = param2.innerText;
+      console.log(cnt2);
+    },
+  },
+}
+</script>
+
+<template>
+  <h1 ref="param1">Do it now.</h1>
+  <h1 ref="param2">Just do it.</h1>
+  <button @click="onClick1">클릭1</button>
+  <button @click="onClick2">클릭2</button>
+</template>
