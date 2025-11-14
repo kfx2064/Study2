4a36b20a14c9b9f4c4aff4075a4467fdb8de9fa5
jeongtaehyun <jth454@naver.com>
2025-11-14 12:45:06 +0900

    vue

---
+++ Vue 입문 강의/ex30/vue/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import account from '@/components/account.vue';
+export default {
+  components: {
+    account,
+  },
+  methods: {
+    addAmount() {
+      alert("추가 입금!");
+    },
+  },
+}
+</script>
+
+<template>
+  <account @add-amount="addAmount" />
+</template>
---
+++ Vue 입문 강의/ex30/vue/src/components/account.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  methods: {
+    parentAddAmount() {
+      this.$emit('add-amount');
+    },
+  },
+}
+</script>
+
+<template>
+  <button type="button" @click="parentAddAmount">클릭</button>
+  <button type="button" @click="$emit('add-amount')">인속성</button>
+</template>
