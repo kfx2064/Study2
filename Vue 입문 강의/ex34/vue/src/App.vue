c85e1058c6423a210ec266c675633bf0d419b53f
jeongtaehyun <jth454@naver.com>
2025-11-14 13:41:44 +0900

    vue

---
+++ Vue 입문 강의/ex34/vue/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Account from '@/components/Account.vue';
+export default {
+  components: {
+    Account,
+  },
+  methods: {
+    click1() {
+      console.log(this.$refs.accountRef.deposit);
+      console.log(this.$refs.accountRef.amount);
+      this.$refs.accountRef.shareAmount();
+    }
+  },
+}
+</script>
+
+<template>
+  <Account ref="accountRef" />
+  <button type="button" @click="click1">클릭</button>
+</template>
---
+++ Vue 입문 강의/ex34/vue/src/components/Account.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      amount: 50000,
+      deposit: '예치금',
+    };
+  },
+  computed: {
+    logGiven() {
+      console.log('배당금 지급');
+    },
+  },
+  methods: {
+    shareAmount() {
+      const addTotalAmount = this.amount + (this.amount * 0.1);
+      console.log("총액 : " + addTotalAmount);
+      this.amount = addTotalAmount;
+      return this.amount;
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>{{ deposit }}</h1>
+  <h1>{{ amount }}</h1>
+</template>
