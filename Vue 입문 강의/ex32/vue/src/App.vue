963f9c927ff89137921609d150cf3142b1ad8b33
jeongtaehyun <jth454@naver.com>
2025-11-14 13:22:31 +0900

    vue

---
+++ Vue 입문 강의/ex32/vue/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import account from '@/components/account.vue';
+export default {
+  components: {
+    account,
+  },
+  provide() {
+    return {
+      deposit: this.deposit,
+      amount: this.amount,
+    };
+  },
+  data() {
+    return {
+      deposit: '예치금',
+      amount: 10000,
+    };
+  },
+}
+</script>
+
+<template>
+  <account />
+</template>
