4a2cc3ab9d3823fc775c39a439ae12bb4b04a4bc
jeongtaehyun <jth454@naver.com>
2025-11-14 13:50:23 +0900

    vue

---
+++ Vue 입문 강의/ex35/vue/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Account from '@/components/Account.vue';
+export default {
+  components: {
+    Account,
+  },
+  data() {
+    return {
+      deposit: '예치금',
+      amount: 20000,
+    };
+  },
+  methods: {
+    shareAmount() {
+      const divide = this.amount * 0.1;
+      this.amount = this.amount - divide;
+      return divide;
+    },
+  },
+}
+</script>
+
+<template>
+  <Account />
+</template>
---
+++ Vue 입문 강의/ex35/vue/src/components/Account.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      childDeposit: '',
+      childAmount: 0,
+      childShareAmt: 0,
+    };
+  },
+  methods: {
+    getData1() {
+      this.childDeposit = this.$parent.deposit;
+      this.childAmount = this.$parent.amount;
+      this.childShareAmt = this.$parent.shareAmount();
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>Account</h1>
+  <h1>{{ childDeposit }}</h1>
+  <h1>{{ childAmount }}</h1>
+  <h1>{{ childShareAmt }}</h1>
+  <button type="button" @click="getData1">클릭</button>
+</template>
