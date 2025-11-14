dbe975115983e0c54edd80487de2881749ddc3a8
jeongtaehyun <jth454@naver.com>
2025-11-14 09:57:01 +0900

    vue study.

---
+++ Vue 입문 강의/ex22/vue/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      team: 'sales',
+      leader: 'John',
+      alpha: 5,
+      bravo: 7,
+    };
+  },
+  computed: {
+    teamDetail() {
+      return `${this.team} - ${this.leader}`;
+    },
+    platoonMember() {
+      return this.alpha + this.bravo;
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>{{ teamDetail }}</h1>
+  <h1>{{ platoonMember }}</h1>
+</template>
---
+++ Vue 입문 강의/ex23/vue/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      pushData: '',
+      arrData: [2, 4, 6],
+      objData: { team: 'alpha' },
+    };
+  },
+  watch: {
+    pushData(newVal, oldVal) {
+      console.log(`oldVal : ${oldVal}, newVal : ${newVal}`);
+    },
+    arrData: {
+      handler(newVal, oldVal) {
+        console.log(`oldVal : ${oldVal}, newVal : ${newVal}`);
+      },
+      deep: true,
+    },
+    objData: {
+      handler(newVal, oldVal) {
+        console.log(`oldVal : ${oldVal}, newVal : ${newVal}`);
+      },
+      deep: true,
+    }
+  },
+}
+</script>
+
+<template>
+  <input type="text" v-model="pushData" />
+  <p>{{ arrData }}</p>
+  <p>{{ objData }}</p>
+  <button @click="arrData.push(10)">데이터 추가</button>
+  <button @click="objData.team = 'bravo'">객체 변경</button>
+</template>
